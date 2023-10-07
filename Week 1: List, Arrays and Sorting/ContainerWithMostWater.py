7f40ac09ec9aee13f81dd27d1efa29b2d9b6563e
Steven Granaturov <60303192+ander9991@users.noreply.github.com>
2023-10-06 22:25:20 -0400

    Update ContainerWithMostWater.py

--- Week 1: List, Arrays and Sorting/ContainerWithMostWater.py
+++ Week 1: List, Arrays and Sorting/ContainerWithMostWater.py
@@ -12,3 +12,20 @@ class Solution:
                     prod = h * x
         return prod
         # TODO: Time Limit Exceeded
+
+
+# Another solution:
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxx = 0 
+        i = 0
+        j = len(height)-1
+        while i < j:
+            width = abs(i-j)
+            area = width * min(height[i],height[j])
+            maxx = max(area,maxx)
+            if height[i] > height[j]:
+                j -=1
+            else:
+                i +=1
+        return maxx 
